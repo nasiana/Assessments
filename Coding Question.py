55f1e838d899f5b2ec03ae3178eb87f25bb008e5
Nasian <87439662+nasiana@users.noreply.github.com>
2021-11-03 15:03:56 +0000

    Add files via upload

--- Coding Question.py
+++ Coding Question.py
@@ -9,17 +9,21 @@ length_array = len(my_numbers)
 my_numbers_compare = my_numbers.copy()
 
 def Two_Number_Sum(my_numbers):
-    leftIdx = 0
-    rightIdx = len(my_numbers) - 1
-    result = []
-    while leftIdx < rightIdx:
-        if my_numbers[leftIdx] + my_numbers[rightIdx] == my_sum:
-            result.append(my_numbers[leftIdx]), int(my_numbers[rightIdx])
-        else:
-             pass
-        leftIdx += 1
-        rightIdx -= 1
-    return result
+    i = 0
+    for i in range(len(my_numbers)):
+        leftIdx = i
+        rightIdx = len(my_numbers) - 1
+        result = []
+        while leftIdx < rightIdx:
+            if my_numbers[leftIdx] + my_numbers[rightIdx] == my_sum:
+                result.append(my_numbers[leftIdx]), int(my_numbers[rightIdx])
+            else:
+                pass
+            rightIdx -= 1
+        return result
+
 
 print(Two_Number_Sum(my_numbers))
 
+my_numbers = [5 ,6 ,9, 3, -2, 7, -4, 5]
+print(my_numbers[3])
