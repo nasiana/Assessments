e0518a5cc6766fc6279649881c6b7131740337c3
Nasian <87439662+nasiana@users.noreply.github.com>
2021-11-03 17:59:05 +0000

    Add files via upload

--- Coding Question.py
+++ Coding Question.py
@@ -3,27 +3,43 @@ Nasian Ahmed
 Coding Question
 """
 
-my_numbers = [5 ,6 ,9, 3, -2, 7, -4, 5]
+# my_numbers = [5, 6, 9, 3, -2, 7, -4, 5]
+# my_sum = 12
+# result = []
+#
+# def Two_Number_Sum(my_numbers):
+#     for i in range(len(my_numbers)):
+#         j = 0
+#         while j < len(my_numbers):
+#             if my_numbers[i] + my_numbers[j] == my_sum and i != j:
+#                 result.append([(my_numbers[i]), (my_numbers[j])])
+#             j += 1
+#         return result
+#
+# print(Two_Number_Sum(my_numbers))
+
+my_numbers = [5, 6, 9, 3, -2, 7, -4, 5]
 my_sum = 12
-length_array = len(my_numbers)
-my_numbers_compare = my_numbers.copy()
+result = []
 
 def Two_Number_Sum(my_numbers):
-    i = 0
     for i in range(len(my_numbers)):
-        leftIdx = i
-        rightIdx = len(my_numbers) - 1
-        result = []
-        while leftIdx < rightIdx:
-            if my_numbers[leftIdx] + my_numbers[rightIdx] == my_sum:
-                result.append(my_numbers[leftIdx]), int(my_numbers[rightIdx])
-            else:
-                pass
-            rightIdx -= 1
-        return result
+        for j in range(len(my_numbers)):
+            if my_numbers[i] + my_numbers[j] == my_sum and i != j:
+                if not [(my_numbers[i]), (my_numbers[j])] in result:
+                    result.append([(my_numbers[i]), (my_numbers[j])])
+    return result
 
+def Sum_Unique():
+    result = Two_Number_Sum(my_numbers)
+    unique_result = {x for l in result for x in l}
+    unique_result_list = list(unique_result)
+    unique_result_formatted = []
+    i = 0
+    while i < len(unique_result):
+        unique_result_formatted.append([unique_result_list[i], unique_result_list[i+1]])
+        i +=2
+    return unique_result_formatted
 
-print(Two_Number_Sum(my_numbers))
+print(Sum_Unique())
 
-my_numbers = [5 ,6 ,9, 3, -2, 7, -4, 5]
-print(my_numbers[3])
